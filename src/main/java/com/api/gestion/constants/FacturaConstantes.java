7aeb40e72789dc4240f8fba73a685da9395f2db0
Yurder Manosalva <yurdermanosalva01@gmail.com>
2025-09-02 11:22:31 -0500

    * Se crearon los archivos y la lógica respectiva para servicios, controladores y modelos con el objetivo de poder conectar a la base de datos y guardar usuarios.

---
+++ src/main/java/com/api/gestion/constants/FacturaConstantes.java
@@ -0,0 +1,8 @@
+package com.api.gestion.constants;
+
+public class FacturaConstantes {
+
+    public static final String SOMETHING_WENT_WRONG = "Algo salió mal";
+
+    public static final String INVALID_DATA = "Datos inválidos";
+}
